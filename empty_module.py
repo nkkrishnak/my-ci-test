d567553404ad2bd697cd88759749b2865d0886c3
John Vandenberg <jayvdb@gmail.com>
2016-05-02 14:54:55 +0700

    Add empty module

---
+++ empty_module.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+"""Empty module."""
+from __future__ import absolute_import, unicode_literals
---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Flake8 putty globre setup module."""
+from __future__ import absolute_import, unicode_literals, with_statement
+
+from setuptools import setup
+
+setup(
+    name='empty_module',
+    version='0.1',
+    description='empty_module.',
+    long_description='empty_module',
+    py_modules=[str('empty_module')],
+)
