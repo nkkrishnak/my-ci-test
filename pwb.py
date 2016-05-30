0e375491cb3723c994f7417b3cd34ae12e1338fd
John Vandenberg <jayvdb@gmail.com>
2016-05-30 07:43:14 +0700

    tidy for branch

--- pwb.py
+++ pwb.py
@@ -3,10 +3,20 @@ u"""Foobar."""
 
 import sys
 
+try:
+    sys.pypy_version_info
+    PYPY = True
+except AttributeError:
+    PYPY = False
+
 #if PYPY:
 #    print('', end='')
 
-sys.path = (u'.', ) + tuple(sys.path[1:])
+def set_sys_path():
+    sys.path = (u'.', ) + tuple(sys.path[1:])
+
+set_sys_path()
 
 if __name__ == '__main__':
-    print(__doc__)
+    if PYPY:
+        print(__doc__)
---
+++ test.sh
@@ -0,0 +1,6 @@
+PYTHONIOENCODING=UTF8 `pwd`/.tox/enc/bin/python pwb.py
+
+python3.4 -m tox -e enc
+
+PYTHONIOENCODING=UTF8 `pwd`/.tox/enc/bin/python pwb.py
+
